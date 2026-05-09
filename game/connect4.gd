ae37241a7ef94ac0f13307d946b962d815a74dda
Fábio <programacao.acronym127@passinbox.com>
2026-05-09 15:27:28 -0300

    Jogador vs jogador

--- game/connect4.gd
+++ game/connect4.gd
@@ -1,57 +1,3 @@
-#extends Node2D
-#
-#var tabuleiro: Tabuleiro = Tabuleiro.new()
-#var ia: Minimax = Minimax.new()
-#var profundidade_maxima = 4
-#
-#func _ready() -> void:
-	#print("Iniciando jogo...")
-	#processar_jogo_auto()
-#
-#func processar_jogo_auto() -> void:
-	#while not tabuleiro.estado_terminal():
-		#mostrar_tabuleiro()
-		#
-		#var jogador = tabuleiro.jogadorAtual
-		#print("Vez do jogador: ", "Amarelo" if jogador.cor == Jogador.Cor.Amarelo else "Vermelho")
-		#
-		#var jogada = ia.jogar(tabuleiro.duplicate(true), jogador, profundidade_maxima)
-		#
-		#if jogada.movimento.is_empty():
-			#print("Sem movimentos disponíveis!")
-			#break
-		#
-		#var linha = jogada.movimento[0]
-		#var coluna = jogada.movimento[1]
-		#
-		#print("Jogada na posição: [", linha, ", ", coluna, "]")
-		#
-		#tabuleiro.computar_jogada(linha, coluna)
-		#if tabuleiro.verificar_vitoria(jogador):
-			#mostrar_tabuleiro()
-			#print("Jogador ", "Amarelo" if jogador.cor == Jogador.Cor.Amarelo else "Vermelho", " venceu!")
-			#break
-	#
-	#if tabuleiro.verificar_empate():
-		#mostrar_tabuleiro()
-		#print("Empate!")
-#
-#func mostrar_tabuleiro() -> void:
-	#print("\nTabuleiro:")
-	#for linha in range(6):
-		#var linha_str = ""
-		#for coluna in range(7):
-			#var cor = tabuleiro.tabuleiro[linha][coluna]
-			#if cor == Jogador.Cor.Nenhum:
-				#linha_str += ". "
-			#elif cor == Jogador.Cor.Amarelo:
-				#linha_str += "A "
-			#else:
-				#linha_str += "V "
-		#print(linha_str)
-	#print("---------------")
-
-
 extends Node2D
 
 @onready var peca_vermelha = "res://Assets/peca_vermelha.png"
@@ -76,7 +22,7 @@ func _ready():
 	$Tabuleiro/Colunas/AreaColuna_5.coluna_selecionada.connect(jogar_na_posicao)
 	$Tabuleiro/Colunas/AreaColuna_6.coluna_selecionada.connect(jogar_na_posicao)
 
-	processar_jogo_auto()
+	#processar_jogo_auto()
 
 	#if pecas_arrastaveis:
 		#for peca_arrastavel in pecas_arrastaveis.get_children():
@@ -110,6 +56,27 @@ func processar_jogo_auto() -> void:
 		print("Empate!")
 
 func jogar_na_posicao(coluna, linha = null):
+	if not tabuleiro_jogo.estado_terminal():
+		var jogador = tabuleiro_jogo.jogadorAtual
+
+		var espaco_disponivel = pegar_espaco_disponivel(coluna, linha)
+		if espaco_disponivel == null:
+			print("Coluna cheia!")
+			return null
+		
+		var peca = peca_cena.instantiate()
+		var cor_peca = peca_amarela if jogador.cor == Jogador.Cor.Amarelo else peca_vermelha
+		peca.mudar_textura_jogador(cor_peca)
+		pecas.add_child(peca)
+	
+		peca.global_position = Vector2(espaco_disponivel.global_position.x, -100)
+		espaco_disponivel.ocupado = true
+		peca.jogar_peca(espaco_disponivel.global_position)
+		
+		tabuleiro_jogo.computar_jogada(espaco_disponivel.linha, espaco_disponivel.coluna)
+		tabuleiro_jogo.verificar_vitoria(jogador)
+
+func pegar_espaco_disponivel(coluna, linha = null):
 	var espaco_disponivel = null
 	var espacos_jogaveis = espacos.get_children()
 	espacos_jogaveis.reverse()
@@ -124,15 +91,4 @@ func jogar_na_posicao(coluna, linha = null):
 				if espaco_disponivel == null or espaco.linha > espaco_disponivel.linha:
 					espaco_disponivel = espaco
 
-	if espaco_disponivel == null:
-		print("Coluna cheia!")
-		return
-
-	var peca = peca_cena.instantiate()
-	var cor_peca = peca_amarela if tabuleiro_jogo.jogadorAtual.cor == Jogador.Cor.Amarelo else peca_vermelha
-	peca.mudar_textura_jogador(cor_peca)
-	pecas.add_child(peca)
-
-	peca.global_position = Vector2(espaco_disponivel.global_position.x, -100)
-	espaco_disponivel.ocupado = true
-	peca.jogar_peca(espaco_disponivel.global_position)
+	return espaco_disponivel
