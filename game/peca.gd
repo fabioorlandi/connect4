9fd6c1a618d1cb444bf5294b334bba000a27c9a3
julioFSch <julio.scheuermann@gmail.com>
2026-05-15 06:33:46 -0300

    Testes de som

--- game/peca.gd
+++ game/peca.gd
@@ -5,7 +5,7 @@ func jogar_peca(posicao: Vector2) -> void:
 	tween.tween_property(self, "global_position:y", posicao.y, 0.6)\
 	.set_trans(Tween.TRANS_BOUNCE)\
 	.set_ease(Tween.EASE_OUT)
-	
+	$Peca_caindo.play()
 	await tween.finished
 
 func mudar_textura_jogador(caminho_arquivo_textura: String) -> void:
