0172d1aeef9f22678e5d3dfb75c648930314274f
JULIO FERREIRA SCHEUERMANN <julio.scheuermann@edu.pucrs.br>
2026-05-15 22:23:40 -0300

    Peça fantasma

--- game/area_coluna.gd
+++ game/area_coluna.gd
@@ -9,3 +9,15 @@ func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if not Global2D.is_dragging:
 				coluna_selecionada.emit(indice_coluna)
+
+
+func _on_mouse_entered() -> void:
+	
+	$AnimatedSprite2D.visible = true 
+	
+	$AnimatedSprite2D.play("peca_fantasma")
+
+
+func _on_mouse_exited() -> void:
+	$AnimatedSprite2D.visible = false 
+	$AnimatedSprite2D.stop()
